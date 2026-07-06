54958126494a21e9a7f25572faaaecccde2caa0d
Pranathi Kommuru <mystudylife786@gmail.com>
2026-07-06 20:26:08 +0530

    feat: initial project setup

---
+++ script.js
@@ -0,0 +1,20 @@
+<div class="container">
+
+    <h1>TaskFlow</h1>
+
+    <p>Simple Task Manager</p>
+
+    <input
+        type="text"
+        id="taskInput"
+        placeholder="Enter a task">
+
+    <button onclick="addTask()">
+        Add Task
+    </button>
+
+    <ul id="taskList"></ul>
+
+</div>
+
+<script src="script.js"></script>
